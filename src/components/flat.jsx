271d2443477e83279d5ff0953d4fd0a879cf0229
Matthew Cwalina <matthewcwalina@gmail.com>
2022-02-04 10:18:06 +0100

    Added new components

---
+++ src/components/flat.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Flat extends Component {
+  render() {
+    return (
+      const src = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg"
+      <img src={src} alt='' className='flat'/>
+    );
+  }
+}
