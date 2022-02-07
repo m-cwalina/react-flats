1ccf638217418a0f3171025b0a183d533b8758f8
Matthew Cwalina <matthewcwalina@gmail.com>
2022-02-07 15:52:28 +0100

    Added google maps with interaction

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 import Flat from './flat.jsx';
-import FlatList from './flat_list.jsx'
-import flats from '../../data/flats.js'
+import FlatList from './flat_list.jsx';
+import Marker from './marker.jsx';
+import flats from '../../data/flats.js';
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +13,13 @@ class App extends Component {
       selectedFlat: flats[0],
       flats
     }
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
   }
 
   selectFlat = (index) => {
@@ -26,10 +34,13 @@ class App extends Component {
           selectedFlat={this.state.selectedFlat}
           selectFlat={this.selectFlat}
         />
-        <div className='map-container'>
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
         </div>
       </div>
-    )
+    );
   }
 }
 
