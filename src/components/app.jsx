4a55f9d07ca3f9e4e0c02ff77c67acddfcb2f8a1
Matthew Cwalina <matthewcwalina@gmail.com>
2022-02-07 15:37:02 +0100

    Added Google Maps to application

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 import Flat from './flat.jsx';
 import FlatList from './flat_list.jsx'
+import flats from '../../data/flats.js'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedflats: flats[0],
+      selectedFlat: flats[0],
       flats
     }
+  }
 
+  selectFlat = (index) => {
+    this.setstate({selectedflats: flats[index]})
   }
 
   render() {
     return (
       <div>
-        <div className='flat-list'>
-          <FlatList flats = {this.state.flats} />
-        </div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
         <div className='map-container'>
         </div>
       </div>
